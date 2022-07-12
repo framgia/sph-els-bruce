14afa087009f755ef2e34202219f86dfdbaa65a0
brucevilo <brucevilo19@gmail.com>
2022-07-12 18:31:33 +0800

    Restore Create user migration

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
             $table->string('lastname');
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('users');
     }
 };
