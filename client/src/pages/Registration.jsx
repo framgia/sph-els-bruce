643098dff2ccf41d57031ba23ac0263c99f67b8b
brucevilo <brucevilo19@gmail.com>
2022-07-19 09:47:16 +0800

    bugfix removing duplicate code

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import UserApi from "../api/UserApi";
-
-
 import swal from "sweetalert";
-
-
-import swal from "sweetalert";
-
 import { useNavigate } from "react-router-dom";
 import UserRegisterApi from "../api/UserRegisterApi";
 
@@ -45,11 +39,8 @@ function Registration({ params }) {
     };
     UserRegisterApi.registerUser(data)
       .then((response) => {
-
-
         localStorage.setItem("auth_token", response.data.token);
         localStorage.setItem("auth_name", response.data.username);
-
 
         window.location = "/dashboard";
       })
